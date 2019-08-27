f36b7b66eaffb67dc1ef9be3bd8881ea8b94a836
Pavel Ardolf <pavel.ardolf@gmail.com>
2019-08-27 18:50:01 +0200

    Prepare navigation, refactoring and bugs repair

--- app/src/main/java/com/sample/smartrestaurants/activities/ShowTopologyActivity.java
+++ app/src/main/java/com/sample/smartrestaurants/activities/ShowTopologyActivity.java
@@ -64,6 +64,13 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
     String name;
     String fName;
 
+    String xPosToilets;
+    String yPosToilets;
+    String xPosCashbox;
+    String yPosCashbox;
+    float[] selectedPlacePosition;
+    float[] otherPlacePosition;
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -107,6 +114,13 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
                         nameRestaurant.setText(name);
                     }
 
+                    xPosToilets = ds.child("xPosToilets").getValue(String.class);
+                    yPosToilets = ds.child("yPosToilets").getValue(String.class);
+                    xPosCashbox = ds.child("xPosCashbox").getValue(String.class);
+                    yPosCashbox = ds.child("yPosCashbox").getValue(String.class);
+
+                    otherPlacePosition = utilsOtherPlace(xPosToilets, yPosToilets, xPosCashbox, yPosCashbox);
+
                     /*
                     String img = ds.child("topology").getValue(String.class);
                     Picasso.get().load(img).into(image);
@@ -141,30 +155,17 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
             @Override
             public void onPhotoTap(View view, float x, float y) {
                 if(isPlaceSelected == false) {
-                    Paint paint = new Paint(Paint.FAKE_BOLD_TEXT_FLAG);
-                    Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.mcdonald);
-                    Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
-                    Canvas tempCanvas = new Canvas(tempBitmap);
-                    height = tempBitmap.getHeight();
-                    width = tempBitmap.getWidth();
-                    tempCanvas.drawBitmap(bitmap, 0, 0, null);
-
-                    x = x * width;
-                    y = y * height;
-                    paint.setColor(Color.rgb(3, 155, 229));
-                    tempCanvas.drawCircle(x, y, 50, paint);
-
-                    Drawable d = new BitmapDrawable(context.getResources(), tempBitmap);
-                    image.setImageDrawable(d);
-                    isPlaceSelected = true;
+                    selectedPlacePosition = selectPlace(x, y);
 
                     AlertDialog.Builder alertPlace = new AlertDialog.Builder(ShowTopologyActivity.this);
                     alertPlace.setTitle("Seating place was selected!");
-                    alertPlace.setMessage("The positions of the other locations will now be loaded..");
+                    alertPlace.setMessage("The positions of the other locations will be loaded now..");
                     alertPlace.setNegativeButton("OK",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.dismiss();
+                                    isPlaceSelected = true;
+                                    updateTopology(context, image, height, width, selectedPlacePosition, otherPlacePosition, isPlaceSelected);
                                 }
                             }).show();
                 }
@@ -209,6 +210,10 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Context context = this;
+        if(isPlaceSelected){
+            //updateTopology(context, image, height, width, selectedPlacePosition, otherPlacePosition);
+        }
         if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             geomagnetic = event.values;
         }
@@ -220,9 +225,13 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
         }
 
         if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            if(isPlaceSelected){
+                updateTopology(context, image, height, width, selectedPlacePosition, otherPlacePosition, isPlaceSelected);
+            }
             isRotationMatrix = SensorManager.getRotationMatrix(r, i, gravity, geomagnetic);
             if(isRotationMatrix) {
                 SensorManager.getOrientation(r, orientation);
+                //updateTopology(context, image, height, width, selectedPlacePosition, otherPlacePosition);
                 azimuth = orientation[0];
             }
         }
@@ -232,5 +241,117 @@ public class ShowTopologyActivity extends AppCompatActivity implements SensorEve
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    public static void updateTopology(Context context, ImageView image, int height, int width, float[] selectedPlacePosition, float[] otherPlacePosition, boolean isPlaceSelected) {
+        Paint paint = new Paint(Paint.FAKE_BOLD_TEXT_FLAG);
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.mcdonald);
+        Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(tempBitmap);
+        height = tempBitmap.getHeight();
+        width = tempBitmap.getWidth();
+        canvas.drawBitmap(bitmap, 0, 0, null);
+
+        //Canvas tempCanvas;
+        drawSelectedPlace(paint, canvas, height, width, selectedPlacePosition, isPlaceSelected);
+        drawOtherPositions(paint, canvas, height, width, otherPlacePosition);
+        drawNavigationLines(paint, canvas, height, width);
+        drawMyPosition(context, paint, canvas, height, width);
+
+        Drawable d = new BitmapDrawable(context.getResources(), tempBitmap);
+        image.setImageDrawable(d);
+    }
+
+    public static float[] selectPlace(float x, float y) {
+        float[] positionPlace = new float[2];
+        positionPlace[0] = x;
+        positionPlace[1] = y;
+
+        return positionPlace;
+    }
+
+    public static void drawSelectedPlace(Paint paint, Canvas canvas, float height, float width, float[] positions, boolean isPlaceSelected){
+        if(isPlaceSelected) {
+            float x = positions[0];
+            float y = positions[1];
+            x *= width;
+            y *= height;
+
+            paint.setColor(Color.rgb(3, 155, 229));
+            canvas.drawCircle(x, y, 50, paint);
+        }
+    }
+
+    public static float[] utilsOtherPlace(String xPosToilets, String yPosToilets, String xPosCashbox, String yPosCashbox) {
+        float[] positionOtherPlace = new float[4];
+        Float xPosFirstParam = Float.parseFloat(xPosToilets);
+        Float yPosFirstParam = Float.parseFloat(yPosToilets);
+        Float xPosSecondParam = Float.parseFloat(xPosCashbox);
+        Float yPosSecondParam = Float.parseFloat(yPosCashbox);
+        positionOtherPlace[0] = xPosFirstParam;
+        positionOtherPlace[1] = yPosFirstParam;
+        positionOtherPlace[2] = xPosSecondParam;
+        positionOtherPlace[3] = yPosSecondParam;
+
+
+        return positionOtherPlace;
+    }
+
+    public static void drawOtherPositions(Paint paint, Canvas canvas, int height, int width, float[] otherPlacePosition) {
+        float xPosFirstParam = otherPlacePosition[0];
+        float yPosFirstParam = otherPlacePosition[1];
+        float xPosSecondParam = otherPlacePosition[2];
+        float yPosSecondParam = otherPlacePosition[3];
+
+        xPosFirstParam = xPosFirstParam * width;
+        yPosFirstParam = yPosFirstParam * height;
+        xPosSecondParam = xPosSecondParam * width;
+        yPosSecondParam = yPosSecondParam * height;
+        paint.setColor(Color.rgb(3, 155, 229));
+        canvas.drawCircle(xPosFirstParam, yPosFirstParam, 50, paint);
+        canvas.drawCircle(xPosSecondParam, yPosSecondParam, 50, paint);
+    }
+
+    public static int[] getCorridorPosition(int posX, int posY){
+        int[] sector = new int[2];
+        int xStart = 49;
+        int yStart = 41;
+
+        for (int height = 0; height < 3;height++) {
+            for (int width = 0; width < 5; width++) {
+                if (posX < width*250+49){
+                    sector[0] = width - 1;
+                    break;
+                }
+            }
+            if (posY < height*817+41){
+                sector[1] = height;
+                sector[1]-= 1;
+                break;
+            }
+        }
+        return sector;
+    }
+
+    public static void drawNavigationLines(Paint paint, Canvas canvas, int height, int width){
+        float x = 0.70500000f;
+        float y = 0.001000000f;
+        x *= width;
+        y *= height;
+
+        paint.setColor(Color.GREEN);
+        paint.setStrokeWidth(2.5f);
+        canvas.drawLine(x, y + 50, x, 1700, paint);
+    }
+
+    public static void drawMyPosition(Context context, Paint paint, Canvas canvas, int height, int width) {
+        float startX = 0.682000000f;
+        float startY = 0.001000000f;
+        startX *= width;
+        startY *= height;
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_location);
+        canvas.drawBitmap(bitmap, startX, startY, paint);
+        //paint.setColor(Color.rgb(0, 255, 0));
     }
 }
